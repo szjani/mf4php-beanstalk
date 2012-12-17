4ab01737d37e87ece999a8f690802e530af7664b
Szurovecz János <szjani@szjani.hu>
2012-12-17 14:03:08 +0100

    Fixed output writing

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,3 +2,5 @@
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     include_once __DIR__ . '/../vendor/autoload.php';
 }
+
+\lf4php\LoggerFactory::setILoggerFactory(new lf4php\nop\NOPLoggerFactory());
